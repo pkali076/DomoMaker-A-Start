a6a41b93982a8409cf146aaeffda3597147d2b1c
pkali076 <77549872+pkali076@users.noreply.github.com>
2022-03-27 02:00:58 -0400

    domomakerB

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -10,7 +10,8 @@ const signupPage = (req, res) => {
 };
 
 const logout = (req, res) => {
-    return res.redirect('/');
+    req.session.destroy();
+    res.redirect('/');
 };
 
 const login = (req, res) => {
@@ -24,6 +25,8 @@ const login = (req, res) => {
         if(err || !account){
             return res.status(401).json({error:' Wrong username or password!'});
         }
+        req.session.account = Account.toAPI(account);
+
         return res.json({redirect: '/maker'});
     });
 };
@@ -44,6 +47,7 @@ const signup = async(req, res) => {
         const hash = await Account.generateHash(pass);
         const newAccount = new Account({username, password: hash});
         await newAccount.save();
+        req.session.account = Account.toAPI(newAccount);
         return res.json({redirect: '/maker'});
 
     }catch(err){
